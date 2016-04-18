1ff06af35726c92f0b28a081980f1594b3457359
lunabitcanny <luna@bitcanny.com>
2016-04-18 18:06:06 +0530

    change in hooks controller

--- app/controllers/api/hooks_controller.rb
+++ app/controllers/api/hooks_controller.rb
@@ -10,7 +10,11 @@ class Api::HooksController < Api::ApiBaseController
   def create
     agent = current_resource_owner
     agent.webhook_url = params[:target_url]
-    agent.category = params[:category] if params[:category].present?
+    if params[:category] == "{{category}}"
+      agent.category = nil
+    else
+      agent.category = params[:category]
+    end
     agent.save
     render json: {id: agent.id}, status: 201
   end
